43c6dc1d09677a37994dbd910e78f1cac26e1e67
Santhosh Kumar M <mskumar@catchpoint.com>
2020-11-25 20:04:45 +0530

    Mutation Update cache

--- src/components/Todo/TodoInput.js
+++ src/components/Todo/TodoInput.js
@@ -3,6 +3,8 @@
 import React, { useState } from "react";
 import { gql, useMutation } from "@apollo/client";
 
+import { GET_MY_TODOS } from "./TodoPrivateList";
+
 const ADD_TODO = gql`
   mutation addTodo($todo: String!, $isPublic: Boolean!) {
     insert_todos(objects: {title: $todo, is_public: $isPublic}) {
@@ -19,14 +21,36 @@ const ADD_TODO = gql`
 
 // eslint-disable-next-line no-unused-vars
 const TodoInput = ({ isPublic = false }) => {
-  const [addTodo] = useMutation(ADD_TODO);
+  const [addTodo] = useMutation(ADD_TODO, {
+    update: updateCache,
+    onCompleted: resetInput
+  });
   const [inputTodo, setInputTodo] = useState("");
+  const resetInput = () => {
+    setInputTodo("");
+  };
+  const updateCache = (cache, {data}) => {
+    if(isPublic) {
+      return null;
+    }
+    
+    const existingTodos = cache.readQuery({
+      query: GET_MY_TODOS
+    });
+
+    const newTodo = data.insert_todos.returning[0];
+    cache.writeQuery({  
+      query: GET_MY_TODOS,
+      data: {todos: [newTodo, ...existingTodos.todos]}
+    });
+  };
   return (
     <form
       className="formInput"
       onSubmit={e => {
         e.preventDefault();
         addTodo({variables: {todo: inputTodo, isPublic}});
+        resetInput();
       }}
     >
       <input className="input" placeholder="What needs to be done?" value={inputTodo} onChange={(e) => setInputTodo(e.target.value)}  />
